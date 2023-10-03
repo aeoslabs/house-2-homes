88b852e34547d9f42bfb094f4fe2b72db2511b21
Nada <nadaaofficial19@gmail.com>
2023-10-03 20:41:20 +0530

    webhook repetation handling fixed

--- app/api/webhook/[id]/route.ts
+++ app/api/webhook/[id]/route.ts
@@ -1,9 +1,7 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
-import { cookies } from 'next/headers';
 import logger from "@/logger";
-import { NextApiRequest } from "next";
-import { supabaseAdmin, updateGenerationStatus } from "@/app/supabase-admin-client";
+import { supabaseAdmin, supabaseStorage, updateGenerationStatus } from "@/app/supabase-admin-client";
+import { v4 } from "uuid";
 
 export async function POST(req: Request, { params }: { params: { id: string } }) {
 
@@ -12,6 +10,7 @@ export async function POST(req: Request, { params }: { params: { id: string } })
     }
 
     const { id } = params
+
     const body = await req.json();
 
     if (!id) {
@@ -24,52 +23,99 @@ export async function POST(req: Request, { params }: { params: { id: string } })
         return sendErrorResponse("No id provided", 400);
     }
 
-    if (!isValidOutput(body)) {
-        return sendErrorResponse("Invalid output format in the body", 400);
-    }
 
     if (body.status !== 'success') {
         const { data: generationsData, error: generationsError } = await updateGenerationStatus(id, body.status)
-        console.log(generationsData.id);
+
         if (generationsError) {
             logger.error({ generationsError, generationsData }, 'error in /api/callback');
             throw new Error('Something went wrong while getting an image');
         }
     }
 
-    try {
+    const output = body.output[1] || body.output[0]
 
-        const { data: generationsData, error: generationsError } = await supabaseAdmin
-            .from('generations')
-            .update({
-                status: 'success',
-                url: body.output[1],
-            })
-            .eq('id', id)
-            .select('*')
+    if (output) {
+        try {
+            const { data: generationsData, error: generationsError } =
+                await supabaseAdmin
+                    .from('generations')
+                    .select('*')
+                    .eq('id', id)
+                    .single();
 
-        if (generationsError) {
-            logger.error({ generationsError, generationsData }, 'error in /api/callback');
-            throw new Error('Something went wrong while saving the image URL');
-        }
-
-        return NextResponse.json({
-            success: true, message: "Image URL saved successfully", body: {
-                generationId: generationsData[0]?.id
+            if (generationsError || !generationsData) {
+                logger.error(
+                    { generationsError, generationsData },
+                    'Error fetching generation'
+                );
+                throw new Error('Error fetching generation');
             }
-        });
-    } catch (error: any) {
-        logger.error(error, 'error in /api/callback');
-        return sendErrorResponse(error.message, 500);
+
+            console.log('generationsData', generationsData)
+            if (generationsData && generationsData.status === 'succeeded') {
+                logger.info({ id }, 'Skipping processing of already processed image');
+                return NextResponse.json({
+                    success: true,
+                    message: "Image already processed",
+                    body: { generationId: id }
+                });
+            }
+
+            const userId = generationsData.user_id;
+
+            const image = await fetch(output);
+            const blob = await image.blob();
+
+            const assetPath = `${userId}/generations/${v4()}`;
+            console.log('assetPath', assetPath)
+            const { error: uploadError } = await supabaseStorage.upload(assetPath, blob, {
+                contentType: 'image/png',
+                cacheControl: '3600',
+            });
+            if (uploadError) {
+                throw new Error(uploadError.message);
+            }
+
+
+            const { data: signedData, error: signError } =
+                await supabaseStorage.createSignedUrl(
+                    assetPath,
+                    60 * 60 * 24 * 365 // 1 year
+                );
+
+            if (signError) {
+                throw new Error(signError.message);
+            }
+
+            const { data: generationsUpdateData, error: generationsUpdateError } = await supabaseAdmin
+                .from('generations')
+                .update({
+                    status: 'success',
+                    url: signedData.signedUrl,
+                })
+                .eq('id', id)
+                .select('*').single()
+
+            if (generationsUpdateError) {
+                logger.error({ generationsUpdateError, generationsUpdateData }, 'error in /api/callback');
+                throw new Error('Something went wrong while saving the image URL');
+            }
+
+            return NextResponse.json({
+                success: true, message: "Image URL saved successfully", body: {
+                    generationId: generationsUpdateData?.id
+                }
+            });
+        }
+        catch (error: any) {
+            logger.error(error, 'error in /api/callback');
+            return sendErrorResponse(error.message, 500);
+        }
     }
-
-
 }
 
 function sendErrorResponse(message: string, status: number) {
     return NextResponse.json({ success: false, message }, { status });
 }
 
-function isValidOutput(body: any): boolean {
-    return body.output && typeof body.output[1] === 'string';
-}
