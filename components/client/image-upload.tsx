f9fe81c9cb8515ac091146eead41345bc7e5093e
Nada <nadaaofficial19@gmail.com>
2023-11-01 19:36:07 +0530

    resol

--- components/client/image-upload.tsx
+++ components/client/image-upload.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import { toast } from "@/hooks/use-toast";
-import { useUser } from "@/hooks/use-user";
 import { postClient } from "@/utils/client-helpers";
 import { fileToBase64 } from "@/utils/helpers";
-import { useRouter } from "next/navigation";
-import { type } from "os";
 import * as React from "react";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Check, CheckCheckIcon } from "lucide-react";
+import { CheckCheckIcon } from "lucide-react";
 import clsx from "clsx";
 import { useFormSelection } from "@/hooks/use-form-selection";
 import Image from "next/image";
 import { Database } from "@/types/supabase";
-import { poppins } from "@/app/fonts";
+import Compressor from "compressorjs";
 
 type Image = Database["public"]["Tables"]["assets"]["Row"];
 
@@ -63,13 +60,24 @@ const ImageUpload = ({ images }: Props) => {
         accept="image/*"
         onChange={(e) => {
           if (e.target.files && e.target.files[0]) {
-            fileToBase64(e.target.files[0])
-              .then((base64Data) => {
-                handleSelectImage(base64Data);
-              })
-              .catch((error) => {
-                console.error("Error converting file to Base64:", error);
-              });
+            new Compressor(e.target.files[0], {
+              quality: 0.8,
+              // The compression process is asynchronous,
+              // which means you have to access the `result` in the `success` hook function.
+              success(result) {
+                // Send the compressed image file to server with XMLHttpRequest.
+                fileToBase64(result)
+                  .then((base64Data) => {
+                    handleSelectImage(base64Data);
+                  })
+                  .catch((error) => {
+                    console.error("Error converting file to Base64:", error);
+                  });
+              },
+              error(err) {
+                console.log(err.message);
+              },
+            });
           }
         }}
         className="hidden"
